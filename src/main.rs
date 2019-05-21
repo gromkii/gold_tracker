d80834ee44898a63b353e53ebc3f16e98c177c8c
Dax <limeside@gmail.com>
2019-05-20 23:18:54 -0500

    Edit main, need to refactor into modules. :rabbit:

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
-// main.rs
-mod input_reader;
+//! main.rs
 
 fn main() {
-    println!("Gold Tracker");
-    println!("Please enter your ammount of gold:");
-    let gold: String = input_reader::get_input();
+    println!("Enter total gold: ");
+    let total_gold: u16 = input_reader::get_parsed_input();
 
-    println!("You are currently holding {}gp.", gold);
+    println!("Total gold held: {}", total_gold);
 }
+
+mod input_reader {
+
+    use std::io;
+
+    pub fn get_parsed_input() -> u16 {
+        parse_u16(get_input())
+    }
+
+    pub fn get_input() -> String {
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)
+            .expect("Could not read line.");
+
+        input
+    }
+
+    pub fn parse_u16(input: String) -> u16 {
+        let trim_input = input.trim();
+        trim_input.parse::<u16>()
+            .expect("Failed to parse input.")
+    }
+}
+
