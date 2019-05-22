02e23c1ca11c37d3138d3689bce7f5782a56098d
Dax <limeside@gmail.com>
2019-05-21 21:49:27 -0500

    Add input module to namespace input_reader() :rabbit:

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 //! main.rs
 
+mod input;
+
+use input::input_reader;
+
 fn main() {
     println!("Enter total gold: ");
     let total_gold: u16 = input_reader::get_parsed_input();
@@ -7,27 +11,5 @@ fn main() {
     println!("Total gold held: {}", total_gold);
 }
 
-mod input_reader {
 
-    use std::io;
-
-    pub fn get_parsed_input() -> u16 {
-        parse_u16(get_input())
-    }
-
-    pub fn get_input() -> String {
-        let mut input = String::new();
-
-        io::stdin().read_line(&mut input)
-            .expect("Could not read line.");
-
-        input
-    }
-
-    pub fn parse_u16(input: String) -> u16 {
-        let trim_input = input.trim();
-        trim_input.parse::<u16>()
-            .expect("Failed to parse input.")
-    }
-}
 
