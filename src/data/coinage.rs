73318fc0acc0ad82085e0d46b9c6f5fba21604ca
Dax Richardson <dax@hangar.com>
2019-05-23 09:29:04 -0500

    Edit coinage, move construct into own impl block. :rabbit:

--- src/data/coinage.rs
+++ src/data/coinage.rs
@@ -4,14 +4,16 @@ pub struct Coinage {
   copper: u32,
 }
 
+// Methods
 impl Coinage {
-
-  pub fn new(gold: u32, silver: u32, copper: u32) -> Coinage {
-    Coinage { gold, silver, copper }
-  }
-
   pub fn get_coinage(&self) -> Vec<u32> {
     vec![self.gold, self.silver, self.copper]
   }
+}
 
+// Constructor
+impl Coinage {
+  pub fn new(gold: u32, silver: u32, copper: u32) -> Coinage {
+    Coinage { gold, silver, copper }
+  }
 }
