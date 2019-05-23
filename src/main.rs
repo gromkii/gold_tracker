5ca4f4709049650be60118dd3a7ed8412a926daf
Dax <limeside@gmail.com>
2019-05-22 21:09:55 -0500

    Refactor, remove Currency struct for now. :rabbit:

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,7 @@ use input::command::Command;
 use data::Currency;
 
 fn main() {
-    init();
-}
-
-fn init() {
-    println!("Enter total gold: ");
-    let total_gold: u16 = input_reader::get_parsed_input();
-    println!("Enter total silver: ");
-    let total_silver: u16 = input_reader::get_parsed_input();
-    println!("Enter total copper: ");
-    let total_copper: u16 = input_reader::get_parsed_input();
-
-    println!("Total gold held: {}", total_gold);
-    println!("Total silver held: {}", total_silver);
-    println!("Total copper held: {}", total_copper);
-
-    let c: Currency = Currency::new(total_gold, total_silver, total_copper);
-
-    println!("Currency: {:?}", c.get_currency());
-
     input_loop();
-    
 }
 
 fn input_loop() {
